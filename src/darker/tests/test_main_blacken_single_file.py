ec8cf343042297d44b10a6d7321d40e2172ac00f
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2022-02-24 09:56:43 +0200

    Refactor resolving path in repo to be done earlier
    It will be reused in isorting.
    
    Also renamed some arguments and variables for clarity.

--- src/darker/tests/test_main_blacken_single_file.py
+++ src/darker/tests/test_main_blacken_single_file.py
@@ -58,6 +58,7 @@ def test_blacken_single_file_common_ancestor(git_repo):
     result = darker.__main__._blacken_single_file(
         git_repo.root,
         Path("a.py"),
+        Path("a.py"),
         EditedLinenumsDiffer(
             git_repo.root,
             RevisionRange.parse_with_common_ancestor("master...", git_repo.root),
